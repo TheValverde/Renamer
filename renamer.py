2df4910947abd171a512735fafe30fcbb005ef7b
Hugo Valverde <hvalverde4800@gmail.com>
2023-03-29 10:53:05 -0700

    Update renamer.py
    Optimized it further. Removed redundant code

--- renamer.py
+++ renamer.py
@@ -1,24 +1,26 @@
 import os
+import re
 import tkinter as tk
 from tkinter import filedialog
 
+def get_prefix_suffix(filename):
+    pattern = r'^(.*)_([\w-]+)_(.*)\..+$'
+    match = re.match(pattern, filename)
+    if match:
+        return match.group(1), match.group(3)
+    return None, None
+
 def batch_rename_files(files, prefix, suffix):
     for file_path in files:
-        file_path = file_path.strip("('")  # Remove unnecessary characters
-        file_path = file_path.rstrip("',")  # Remove trailing characters
         directory, filename = os.path.split(file_path)
-        filename_parts = filename.split('_')
-        if len(filename_parts) > 2:
-            current_prefix, name, current_suffix_ext = filename_parts[0], filename_parts[1], '_'.join(filename_parts[2:])
-            current_suffix, ext = os.path.splitext(current_suffix_ext)
+        current_prefix, current_suffix = get_prefix_suffix(filename)
+        
+        if current_prefix == prefix and current_suffix == suffix:
+            print(f"Skipped {file_path} (already has the desired prefix and suffix)")
+            continue
 
-            # Skip the file if the current prefix and suffix match the desired ones
-            if current_prefix == prefix and current_suffix == suffix:
-                print(f"Skipped {file_path} (already has the desired prefix and suffix)")
-                continue
-
-        filename, ext = os.path.splitext(filename)
-        new_filename = f"{prefix}_{filename}_{suffix}{ext}"
+        name, ext = os.path.splitext(filename)
+        new_filename = f"{prefix}_{name}_{suffix}{ext}"
         new_file_path = os.path.join(directory, new_filename)
         os.rename(file_path, new_file_path)
         print(f"Renamed {file_path} to {new_file_path}")
@@ -26,27 +28,27 @@ def batch_rename_files(files, prefix, suffix):
 def edit_prefix_suffix(files, new_prefix, new_suffix):
     for file_path in files:
         directory, filename = os.path.split(file_path)
-        parts = filename.split("_")
-        if len(parts) > 2:
-            current_prefix, name, current_suffix_ext = parts[0], parts[1], '_'.join(parts[2:])
-            current_suffix, ext = os.path.splitext(current_suffix_ext)
+        current_prefix, current_suffix = get_prefix_suffix(filename)
 
-            # Skip the file if the current prefix and suffix match the desired ones
-            if current_prefix == new_prefix and current_suffix == new_suffix:
-                print(f"Skipped {file_path} (already has the desired prefix and suffix)")
-                continue
+        if current_prefix == new_prefix and current_suffix == new_suffix:
+            print(f"Skipped {file_path} (already has the desired prefix and suffix)")
+            continue
 
+        if current_prefix is not None and current_suffix is not None:
+            name, ext = os.path.splitext(filename.split('_', 1)[-1].rsplit('_', 1)[0])
             new_filename = f"{new_prefix}_{name}_{new_suffix}{ext}"
             new_file_path = os.path.join(directory, new_filename)
             os.rename(file_path, new_file_path)
             print(f"Renamed {file_path} to {new_file_path}")
-              
+
+def browse_files():
+    files = filedialog.askopenfilenames()
+    files_var.set(files)
+
 def start_editing():
-    print("Starting editing...")
-    raw_file_paths = files_var.get()
-    files = raw_file_paths.split("', '")  # Split the paths using the custom delimiter
-    files[0] = files[0].lstrip("('")  # Remove leading characters from the first file path
-    files[-1] = files[-1].rstrip("',)")  # Remove trailing characters from the last file path
+    files = files_var.get().split("', '")
+    files[0] = files[0].lstrip("('")
+    files[-1] = files[-1].rstrip("',)")
 
     new_prefix = prefix_var.get()
     new_suffix = suffix_var.get()
@@ -55,28 +57,6 @@ def start_editing():
         edit_prefix_suffix(files, new_prefix, new_suffix)
     else:
         print("No files selected. Please try again.")
-
-            
-def browse_files():
-    files = filedialog.askopenfilenames()
-    files_var.set(files)
-
-def start_renaming():
-    print("Starting renaming...")
-    raw_file_paths = files_var.get()
-    files = raw_file_paths.split("', '")  # Split the paths using the custom delimiter
-    files[0] = files[0].lstrip("('")  # Remove leading characters from the first file path
-    files[-1] = files[-1].rstrip("',)")  # Remove trailing characters from the last file path
-
-    prefix = prefix_var.get()
-    suffix = suffix_var.get()
-
-    if files:
-        batch_rename_files(files, prefix, suffix)
-    else:
-        print("No files selected. Please try again.")
-
-
 
 root = tk.Tk()
 root.title("Batch Rename Files")
@@ -94,9 +74,7 @@ tk.Entry(root, textvariable=prefix_var).grid(row=1, column=1)
 tk.Label(root, text="Suffix:").grid(row=2, column=0, sticky="e")
 tk.Entry(root, textvariable=suffix_var).grid(row=2, column=1)
 
+tk.Button(root, text="Rename", command=start_editing).grid(row=3, column=0, sticky="e")
 
-tk.Button(root, text="Edit Prefix/Suffix", command=start_editing).grid(row=3, column=2)
-
-tk.Button(root, text="Start Renaming", command=start_renaming).grid(row=3, column=1)
 
 root.mainloop()
